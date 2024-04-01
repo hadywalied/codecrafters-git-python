8b35e2d57311e86fd395059b6adf340c0f90aaa3
Hady Walied <hadywaliedkamel@gmail.com>
2024-04-01 17:51:19 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import sys
 import os
+import zlib, re
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
     #
@@ -16,6 +17,17 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+
+    elif command == "cat-file":
+        option = sys.argv[2]
+        file = sys.argv[3]
+        if option == "-p":
+            with open(f".git/objects/{file[:2]}/{file[2:]}", "rb") as file:
+                content = file.read()
+            decompressed_content = zlib.decompress(content)
+            text_data = decompressed_content.decode("utf-8")
+            filtered_data = re.sub(r"^.*?\x00", "", text_data)
+            print(filtered_data, end="")
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
